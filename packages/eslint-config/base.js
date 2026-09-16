53ccc0ee2f70c52ae9c7abb14064929a0d61c3f1
Hadiuzzaman Bappy <dbappy411@gmail.com>
2026-09-16 20:15:41 +0600

    Initial turbo monorepo ulti project management project boilerplate created with shadcn and tailwind

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+
+/**
+ * A shared ESLint configuration for the repository enforcing strict TypeScript checks.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const config = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
+      "no-unused-vars": "off"
+    },
+  },
+  {
+    ignores: ["dist/**", ".next/**", "node_modules/**"],
+  },
+];
